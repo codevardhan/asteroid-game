4b85365d8cdb0a923e6ebfa7763d7067f0e0f5ac
Charles Carlson <carlson.charles20@gmail.com>
2025-03-16 17:17:39 -0400

    added timer, and collection of environment data

--- asteroid-random/main.py
+++ asteroid-random/main.py
@@ -1,6 +1,6 @@
 import json
-import pygame
-import pygame.freetype
+import pygame # type: ignore
+import pygame.freetype # type: ignore
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -8,6 +8,7 @@ from asteroidfield import *
 from shot import Shot
 from powerups import PowerUp
 player_data = []
+environment_data = []
 def collect_player_data(player,shots,score):
     state = {
         "player_x":player.position.x,
@@ -23,17 +24,26 @@ def collect_player_data(player,shots,score):
     }
     player_data.append(state)
 
+def collect_environment_data(asteroids,time,powerups):
+    state = {
+        "num_powerups_on_board":len(powerups),
+        "num_asteroids_in_field":len(asteroids),
+        "current_play_time":time
+    }
+    environment_data.append(state)
 def save_data():
-    with open("training_data.json", "w") as f:
+    with open("player_training_data.json", "w") as f:
         json.dump(player_data, f, indent=4)
 
+    with open("environment_training_data.json","w") as e:
+        json.dump(environment_data,e,indent=4)
 def main():
     #initialzing  game and screen
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     font = pygame.freetype.SysFont(None, 36)
-
+    start_time = pygame.time.get_ticks()
     #groups
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
@@ -114,9 +124,11 @@ def main():
                 powerup.apply_effect(player)
                 lives = player.player_lives
                 powerup.remove()
-
+        elapsed_time = (pygame.time.get_ticks() - start_time) // 1000
         font.render_to(screen, (10,10), f"Score: {score}", (255,255,255))
         font.render_to(screen, (180,10),f"Lives: {lives}",(255,255,255))
+        font.render_to(screen,(SCREEN_WIDTH - 300,10),f"Time: {elapsed_time}",(255,255,255))
+        collect_environment_data(asteroids,elapsed_time,powerups)
         collect_player_data(player,shots,score)
         if game_over:
             font.render_to(screen, (SCREEN_WIDTH //2 -100, SCREEN_HEIGHT // 2), "GAME OVER", (255,255,255))
