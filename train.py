a826c77674145ba1dff794ec3ea3f745fcd518d9
codevardhan <harsha7vardhan@gmail.com>
2025-04-17 16:24:38 -0400

    update test, train, code

--- train.py
+++ train.py
@@ -2,32 +2,52 @@ import wandb
 from wandb.integration.sb3 import WandbCallback
 
 import sys
-sys.path.insert(0, 'pcg-agents')
 
-from pcgrl import AsteroidsPCGEnvWithAStar
+sys.path.insert(0, "pcg-agents")
+
+from pcgrl_koster_powerups import AsteroidsPCGEnvKoster
 from stable_baselines3 import PPO
 
 
 def main():
-    wandb.init(project="asteroids-pcg", name="basic-pcg-run")
-    env = AsteroidsPCGEnvWithAStar(
-        render_mode=None, max_steps=600, spawn_limit=3)
+    wandb.init(
+        project="asteroids-pcg",
+        name="basic-pcg-run",
+        config={
+            "algo": "PPO",
+            "total_timesteps": 200_000,
+            "env_max_steps": 600,
+            "spawn_limit": 3,
+            "gamma": 0.99,
+            "learning_rate": 3e-4,
+            "gae_lambda": 0.95,
+            "n_steps": 2048,
+            "ent_coef": 0.0,
+            "clip_range": 0.2,
+        },
+        sync_tensorboard=True,
+    )
+    env = AsteroidsPCGEnvKoster(render_mode=None, max_steps=600, spawn_limit=3)
 
     # Create a PPO model
-    model = PPO("MlpPolicy", env, verbose=1, device="cpu")
+    model = PPO("MlpPolicy", env, verbose=1, device="cpu", tensorboard_log="tb/")
 
     # Train
-    model.learn(total_timesteps=200000, callback=WandbCallback(
-        gradient_save_freq=1000,
-        model_save_freq=5000,
-        model_save_path="models/",
-        verbose=2
-    ))
+    model.learn(
+        total_timesteps=500000,
+        callback=WandbCallback(
+            gradient_save_freq=1000,
+            model_save_freq=5000,
+            model_save_path="models/",
+            verbose=2,
+        ),
+    )
 
     # Save
     model.save("outputs/asteroids_pcg_model")
 
     env.close()
+    wandb.finish()
 
 
 if __name__ == "__main__":
